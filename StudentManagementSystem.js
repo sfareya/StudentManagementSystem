a4d5f27c91d1dfda930539ccfe4bdd3b06fed422
Eya Sfar <eya.sfar@medtech.tn>
2021-09-27 11:16:55 +0100

    modified index, student.js

--- StudentManagementSystem.js
+++ StudentManagementSystem.js
@@ -1,1 +1,39 @@
+var numberOfStudents = 0;
+var students = new Array();
+var firstName;
+var lastName;
 
+function createFullName(firstName, lastName) {
+  return ` ${firstName} + " " + ${lastName}`;
+}
+
+function addStudent() {
+  students.push(name);
+  numberOfStudents = students.length;
+}
+
+//function getStudentByInitials(firstInitials, secondInitial) {
+//  while ()
+//}
+
+function isFullName(name) {
+  var hehe = name.split(' ')
+  if (hehe.length > 1) {
+    return true
+  }
+}
+
+function getNumberOfStudents() {
+  return numberOfStudents;
+}
+
+function clearStudents() {
+  students.length = 0;
+  numberOfStudents = 0;
+}
+
+addStudent(createFullName("Josh","Bull"))
+addStudent(createFullName("John","Doe"))
+addStudent(createFullName("Jane","Doe"))
+addStudent(createFullName("Herlock","Sholmes"))
+addStudent(createFullName("Mia","Fey"))
